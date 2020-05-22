0d9a07086fbc6e95d4bd98d72fc87048a6720393
Magne <magne.holan@gmail.com>
2020-05-22 02:09:20 +0200

    react

--- commands/klage.js
+++ commands/klage.js
@@ -2,5 +2,6 @@ const moment = require('moment')
 const _ = require('lodash')
 
 module.exports = async (msg, content, { query }) => {
+  msg.react('👮')
   query.registerComplaint({ channelid: msg.channel.id, messageid: msg.id })
 }
