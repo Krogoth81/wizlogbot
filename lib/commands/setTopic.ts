20bade3cf9d943acbe8e4aae0c816322507200ed
Krogoth81 <magne.holan@gmail.com>
2023-11-23 00:25:16 +0100

    added cron job for dragongate meetings

--- lib/commands/setTopic.ts
+++ lib/commands/setTopic.ts
@@ -1,5 +1,10 @@
+import schedule from 'node-schedule'
+import dayjs from 'dayjs'
 import { ChannelType, TextChannel } from 'discord.js'
 import { MessageResolver } from 'lib/types'
+import { nanoid } from 'nanoid'
+
+const jobs: Array<{ id: string; job: schedule.Job; channelId: string }> = []
 
 export const setTopic: MessageResolver = async (msg, content) => {
   try {
@@ -23,6 +28,35 @@ export const setTopic: MessageResolver = async (msg, content) => {
 
       if (rateLimited) {
         msg.reply(`HALP! I'm being rate limited. The topic change will happen, it might just take a few minutes!`)
+      }
+
+      if (msg.channel.name.toLocaleLowerCase() === 'dragongate') {
+        const regex = /\s(\d{1,2}\/\d{1,2})(?:\s|$)/g
+        const match = content.match(regex)
+
+        if (match?.[0]) {
+          if (jobs.find((j) => j.channelId === msg.channelId)) {
+            jobs.splice(
+              jobs.findIndex((j) => j.channelId === msg.channelId),
+              1,
+            )
+          }
+
+          const [day, month] = match[0].trim().split('/')
+          const date = dayjs().set('date', Number(day)).set('month', Number(month)).set('hour', 12).toDate()
+          const id = nanoid(10)
+          const job = schedule.scheduleJob(date, () => {
+            msg.channel.send(
+              `${
+                content.split(' ')[0]
+              } Husk møte i kveld! :comet::smiley_cat:  :squid::person_raising_hand::squid:  :shield::man_beard:  :dagger::smirk_cat:`,
+            )
+          })
+          if (job) {
+            msg.react('📅')
+            jobs.push({ id, job, channelId: msg.channelId })
+          }
+        }
       }
     }
   } catch {
