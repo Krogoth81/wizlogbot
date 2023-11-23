c5a6679b581515cc24544fc03035777dbeab930e
Krogoth81 <magne.holan@gmail.com>
2023-11-23 20:12:14 +0100

    printing out error message for settopic

--- lib/commands/setTopic.ts
+++ lib/commands/setTopic.ts
@@ -11,24 +11,12 @@ export const setTopic: MessageResolver = async (msg, content) => {
   try {
     if (msg.channel.type === ChannelType.GuildText) {
       const channel = msg.channel as TextChannel
-      channel
-        .setTopic(content)
-        .then(() => {
-          msg.react('👍')
-        })
-        .catch((err) => {
-          console.error(err)
-        })
-
-      const rateLimited = await new Promise((resolve) => {
-        setTimeout(() => {
-          const topic = channel.topic
-          resolve(topic !== content)
-        }, 2000)
-      })
-
-      if (rateLimited) {
-        msg.reply(`HALP! I'm being rate limited. The topic change will happen, it might just take a few minutes!`)
+      try {
+        await channel.setTopic(content)
+        msg.react('👍')
+      } catch (err) {
+        console.error(err)
+        msg.reply(`Sorry mac, topic wasn't set! :rolling_eyes:\n**Here's why:**\n\`\`\`${err.message}\`\`\``)
       }
 
       if (msg.channel.name.toLocaleLowerCase() === 'dragongate') {
