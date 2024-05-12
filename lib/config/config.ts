71b93d7533b8822ea1907cca03ae97be9a899e94
Krogoth81 <magne.holan@gmail.com>
2024-05-12 10:10:57 +0200

    adding mongodb and agenda libs

--- lib/config/config.ts
+++ lib/config/config.ts
@@ -3,4 +3,8 @@ export const config = {
   vinmonopolKey: process.env.VINMONOPOLKEY,
   ownerId: process.env.OWNERID,
   isProd: process.env.NODE_ENV === 'production',
+  mongo: {
+    user: process.env.MONGODB_USER,
+    password: process.env.MONGODB_PASSWORD,
+  },
 }
