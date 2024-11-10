d10f607d40088fa9e42d697cca6e0696d671ba02
Krogoth81 <magne.holan@gmail.com>
2024-11-10 14:27:14 +0100

    updating echo command to echome

--- lib/commands/echo.ts
+++ lib/commands/echo.ts
@@ -1,8 +1,8 @@
 import { MessageResolver } from 'lib/types'
 
-export const echo: MessageResolver = async (msg, content) => {
+export const echome: MessageResolver = async (msg, content) => {
   try {
-    msg.channel.send(content)
+    msg.reply(`Echoing:\n${content}`)
   } catch (err) {
     await msg.reply(`Sorry mac, Can't do that! :rolling_eyes:\n**Here's why:**\n\`\`\`${err.message}\`\`\``)
   }
