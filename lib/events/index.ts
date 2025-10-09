6eb2f8670cb80151684c5d176e57239937ebaea0
Krogoth81 <magne.holan@gmail.com>
2025-10-09 13:53:00 +0200

    adding rate limiter for event triggers

--- lib/events/index.ts
+++ lib/events/index.ts
@@ -4,6 +4,7 @@ import { drinkmore } from './drinkmore'
 import { mountain } from './mountain'
 import { newthing } from './newthing'
 import { oled } from './oled'
+import dayjs from 'dayjs'
 
 interface TextEvent {
   key: string
@@ -41,13 +42,35 @@ const eventsList: Array<TextEvent> = [
   },
 ]
 
+interface TriggerData {
+  user: string
+  key: string
+  timestamp: number
+}
+
+const RATE_LIMIT_TIME_IN_SECONDS = 600
+const previousTriggers: Array<TriggerData> = []
+
 export const events: MessageResolver = async (msg, __, context) => {
   const dipsoUser = msg.author?.username?.toLowerCase().includes('dipso')
   const content = msg.content
+  const rateLimitTimestamp = dayjs().subtract(RATE_LIMIT_TIME_IN_SECONDS, 'seconds').valueOf()
   const events = eventsList.filter(
     (ev) => msg.content.match(ev.regex) || (dipsoUser && ev.dipsoRegex && msg.content.match(ev.dipsoRegex)),
   )
   for (const event of events) {
-    event?.run(msg, content, context)
+    const prevTriggerIndex = previousTriggers.findIndex(pt => pt.user === msg.author.id && pt.key === event.key && rateLimitTimestamp < pt.timestamp)
+    if (prevTriggerIndex < 0) {
+      event?.run(msg, content, context)
+      if (prevTriggerIndex === -1) {
+        previousTriggers.push({
+          key: event.key,
+          timestamp: dayjs().valueOf(),
+          user: msg.author.id
+        })
+      } else {
+        previousTriggers[prevTriggerIndex].timestamp = dayjs().valueOf()
+      }
+    }
   }
 }
